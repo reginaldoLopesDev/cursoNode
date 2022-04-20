cb19e14f66efd4af314e87cbd28cd843614a0d61
Reginaldo Lopes Dev <reginaldo.lopes.dev@gmail.com>
2022-04-20 19:20:26 -0300

    usando mais do módulo

--- modulo2/aula1/index.js
+++ modulo2/aula1/index.js
@@ -1,3 +1,8 @@
 const moduloExterno = require("./aula1");
 
 moduloExterno(35, 72);
+moduloExterno(765, 76);
+moduloExterno(73, 22);
+moduloExterno(157, 999);
+moduloExterno(1209, 114);
+moduloExterno(1574, 18);
